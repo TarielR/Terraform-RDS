4559b9c7f553ead88329b3281e90f7f5c7fcd4ec
Tariel <tar.iel.reg@gmail.com>
2023-03-07 21:42:04 -0500

    Global cluster added

--- cluster.tf
+++ cluster.tf
@@ -1,7 +1,15 @@
+resource "aws_rds_global_cluster" "global" {
+  global_cluster_identifier = "aurora-global"
+  engine                    = "aurora-postgresql"
+  engine_version            = "14.4"
+  database_name             = "example"
+}
+
 resource "aws_rds_cluster" "postgresql" {
+  global_cluster_identifier = aws_rds_global_cluster.global.id
   cluster_identifier      = "aurora-cluster"
-  engine                  = "aurora-postgresql"
-  engine_version          = "14.4"
+  engine                  = aws_rds_global_cluster.global.engine
+  engine_version          = aws_rds_global_cluster.global.engine_version
   vpc_security_group_ids  = [module.vpc_rds.vpc_sg_id]
   availability_zones      = ["us-east-1b", "us-east-1c"]
   database_name           = var.db_name
@@ -16,15 +24,15 @@ resource "aws_rds_cluster" "postgresql" {
 }
 
 resource "aws_db_subnet_group" "dbsg" {
-  name = "private"
+  name = "rds_subnet_group"
   subnet_ids = [module.vpc_rds.vpc_subnet1, module.vpc_rds.vpc_subnet2]
 }
 
 resource "aws_rds_cluster_instance" "cluster_instances" {
   count              = length(aws_db_subnet_group.dbsg.subnet_ids)
-  identifier         = "aurora-cluster-demo-${count.index}"
+  identifier         = "instance-${count.index}"
   cluster_identifier = aws_rds_cluster.postgresql.id
-  instance_class     = "db.t4g.medium"
+  instance_class     = "db.r5.large"
   engine             = aws_rds_cluster.postgresql.engine
   engine_version     = aws_rds_cluster.postgresql.engine_version
   depends_on = [aws_db_subnet_group.dbsg]
@@ -38,24 +46,28 @@ variable "db_name" {
 }
 
 ###########################
-output "cluster_availability_zones" {
-  value = aws_rds_cluster.postgresql.availability_zones
+output "db_subnet_group_id" {
+  value = aws_db_subnet_group.dbsg.subnet_ids
 }
 
-output "cluster_port" {
-  value = aws_rds_cluster.postgresql.port
-}
-
-output "db_subnet_group_name" {
-  value = aws_rds_cluster.postgresql.db_subnet_group_name
-}
-
-output "cluster_endpoint" {
-  value = aws_rds_cluster.postgresql.endpoint
-}
-
-output "cluster_members" {
-  value = aws_rds_cluster.postgresql.cluster_members
-}
+//output "cluster_availability_zones" {
+//  value = aws_rds_cluster.postgresql.availability_zones
+//}
+//
+//output "cluster_port" {
+//  value = aws_rds_cluster.postgresql.port
+//}
+//
+//output "db_subnet_group_name" {
+//  value = aws_rds_cluster.postgresql.db_subnet_group_name
+//}
+//
+//output "cluster_endpoint" {
+//  value = aws_rds_cluster.postgresql.endpoint
+//}
+//
+//output "cluster_members" {
+//  value = aws_rds_cluster.postgresql.cluster_members
+//}
 
 
